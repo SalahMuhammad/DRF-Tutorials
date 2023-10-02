5588fb6b4aba4482c53ebd7e7211f09786a26a2a
SalahMuhammad <salahnazeer93@gmail.com>
2023-10-02 10:24:17 +0300

    .

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+# 
+from rest_framework.routers import DefaultRouter
+from snippets.views import SnippetViewSet
+from users.views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet, basename="snippet")
+router.register(r'users', UserViewSet, basename="user")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,5 +25,5 @@ urlpatterns += [
 
 # api root
 urlpatterns += [
-    path('', include('api.urls'))
+    path('', include(router.urls))
 ]
